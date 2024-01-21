c220a11d4ad6eb69751870ac01bfb6bcb4b5a4bc
Vamsi Krishna Aitha <ivamsi2001@gmail.com>
2024-01-21 11:37:06 +0530

    Perf Check without MySQL Dependent

--- src/main/java/com/venkat/controllers/UserMasterController.java
+++ src/main/java/com/venkat/controllers/UserMasterController.java
@@ -2,7 +2,6 @@ package com.venkat.controllers;
 
 import com.venkat.entity.User;
 import com.venkat.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping(path = "/user")
 public class UserMasterController {
 
     private final UserRepository userRepository;
+
+    public UserMasterController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping(path = "/add")
     public User addNewUser(@RequestParam String name, @RequestParam String email) {
--- src/main/java/com/venkat/entity/User.java
+++ src/main/java/com/venkat/entity/User.java
@@ -1,12 +1,5 @@
 package com.venkat.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 public class User {
     private Integer id;
     private String name;
@@ -14,6 +7,30 @@ public class User {
 
     public User(String name, String email) {
         this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
